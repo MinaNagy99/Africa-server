e11c5fb587f2038d09ffe6cb16b2b9a41e065f85
MinaNagy99 <minanagykhalefa@gmail.com>
2023-12-11 16:39:58 +0200

    dfs

--- server.js
+++ server.js
@@ -1,45 +1,55 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-const port = 3000;
+const port = 3001;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(cors());
-app.use(express.json());
 
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: "info@cts-egy.com", // Replace with your Gmail email address
-        pass: "ccfn fhvr tszn wxvi", // Replace with your Gmail password or app-specific password
-    },
+// Create a reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "info@cts-egy.com", // your Gmail email
+    pass: "ccfn fhvr tszn wxvi",
+  },
 });
 
-app.post("/submit", async (req, res) => {
-    const { name, email, phone, message } = req.body;
+app.post("/submit", (req, res) => {
+  const {  name, phone, email, message } = req.body;
 
-    const mailOptions = {
-        from: "info@cts-egy.com", // Replace with your Gmail email address
-        to: "minanagykhalefa@gmial.com", // Replace with the recipient's email address
-        subject: "New Africa Queen Form Submission",
-        html: `
-      <p><strong>Name:</strong> ${name}</p>
-      <p><strong>Email:</strong> ${email}</p>
-      <p><strong>Phone:</strong> ${phone}</p>
-      <p><strong>Message:</strong> ${message}</p>
-    `,
-    };
+  let mailOptions = {
+    from: "info@cts-egy.com",
+    to: "kamel.mamdouh@cts-egy.com",
+    subject: "New Africa Queen Form Submission",
+    html: `
+  <p><strong>Name:</strong> ${name}</p>
+  <p><strong>Email:</strong> ${email}</p>
+  <p><strong>Phone:</strong> ${phone}</p>
+  <p><strong>Message:</strong> ${message}</p>
+`,
+  };
 
-    try {
-        await transporter.sendMail(mailOptions);
-        res.status(200).json({ message: "Form submitted successfully" });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Failed to submit form" });
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error sending email:", error);
+      return res.status(500).json({
+        success: false,
+        error: "Failed to send email",
+        details: error,
+      });
+    } else {
+      console.log("Message sent: %s", info.messageId);
+      return res.status(200).json({ success: true });
     }
+  });
 });
 
-app.listen(process.env.PORT||port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
 });
